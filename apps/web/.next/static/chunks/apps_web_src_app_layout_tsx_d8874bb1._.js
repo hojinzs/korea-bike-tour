67bae0eb5a97ba43b7b8d2baae6c02e738de20e7
Steve Lee <stevelee@Steves-iMac.local>
2026-03-27 13:08:32 +0900

    feat: add Four Rivers Trail GeoJSON route data
    Source: rollingexistence.com (GPX, free use)
    - 7,091 track points downsampled to 507 for web performance
    - Covers Seoul to Busan via 4 rivers (~400 miles)

--- apps/web/.next/static/chunks/apps_web_src_app_layout_tsx_d8874bb1._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules__pnpm_fa9231b0._.js",
-  "static/chunks/apps_web_src_app_globals_ea8a3ee7.css"
-],
-    source: "dynamic"
-});
